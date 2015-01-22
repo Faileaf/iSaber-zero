681508518bde03442128c8480ebf4b3133771bdb
Faileaf <saberlove@me.com>
2015-01-23 00:23:24 +0800

    创建自己的项目
    根据thinkcmf 1.5.0进行二次开发

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_NAME' => 'test',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'sp_',
+    //密钥
+    "AUTHCODE" => 'G37PxA7iv0HeFAfuJo',
+    //cookies
+    "COOKIE_PREFIX" => 'GuvlzB_',
+);
+?>
